ac162912e47a877f753e19f0673f8c21f9475c22
Subhash kumar <kumawatsubhash767@gmail.com>
2022-12-03 19:11:10 +0530

    done

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ function validform(){
     let xlength = x.length
     console.log(xlength)
 
-  
-}
-
-
+    
     let y = document.querySelector('#email').value
     console.log(y)
 
     let ylength = y.length
     console.log(ylength)
+
+
+    let emailStructure = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    console.log(emailStructure)
 
     let z = document.querySelector('#password').value
     console.log(z)
@@ -23,24 +24,28 @@ function validform(){
     let zlength = z.length
     console.log(zlength)
 
+  
+}
 
 
-if(x_Length <=3){
+
+
+if(xlength <=3){
     document.querySelector('.input_wrong').innerText = 'User name should contain 3 character...'
 }
 else if(!y.match(emailStructure)){
     document.querySelector('.input_wrong').innerText = 'Enter valid e-mail...'
 }
-else if (z_Length <=0){
+else if (zlength <=0){
     document.querySelector('.input_wrong').innerText = 'Password is required......'
 }
-else if(z_Length <=5){
+else if(zlength <=5){
     document.querySelector('.input_wrong').innerText = 'Password is too Short.....'
 }
-else if(z_Length <=10){
+else if(zlength <=10){
     document.querySelector('.input_wrong').innerText = 'Password is Moderate.....'
 }
-else if(z_Length <=20){
+else if(zlength <=20){
     document.querySelector('.input_wrong').innerText = ''
     document.querySelector('.input_success').innerText = 'Password is Strong & Submitted '
 }
